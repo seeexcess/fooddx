4a1342043aa8436f5b7fe6eea3b99ed63f107c21
seeexcess <lanreattiogbe@gmail.com>
2022-06-28 19:27:28 +0100

    finished the subscribe section

--- src/sections/Subscribe/Subscribe.js
+++ src/sections/Subscribe/Subscribe.js
@@ -4,7 +4,18 @@ function Subscribe() {
   return (
     <section className={classes.subscribe}>
       <div className={`${classes.subscribe__container} ${classes.container}`}>
-        Subscribe
+        <h2 className={classes.subscribe__heading}>
+          Hurry up! Subscribe our newsletter and get 25% Off
+        </h2>
+        <p className={classes.subscribe__desc}>
+          Limited time offer for this month. No credit card required.
+        </p>
+        <form action="#" className={classes.subscribe__form}>
+          <input type="email" placeholder="Email Address Here" className={classes.subscribe__input} />
+          <button className={`${classes.subscribe__btn} ${classes.btn}`}>
+            Subscribe
+          </button>
+        </form>
       </div>
     </section>
   )
